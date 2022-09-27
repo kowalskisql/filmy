e8a34520f368f84d67a9124fc5cad1414a043bb0
Kowalski <kowalski@kursysql.com.pl>
2022-09-27 21:30:02 +0000

    Add column GatunekEN

--- Tables/Gatunek.sql
+++ Tables/Gatunek.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Gatunek] (
     [GatunekID] INT        IDENTITY (1, 1) NOT NULL,
     [Gatunek]   NVARCHAR (100) NULL,
+	[GatunekEN]   NVARCHAR (100) NULL,
     PRIMARY KEY CLUSTERED ([GatunekID] ASC)
 );
 
